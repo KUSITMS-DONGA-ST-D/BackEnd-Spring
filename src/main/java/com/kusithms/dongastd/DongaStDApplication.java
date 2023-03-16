057311b5ddea4a92d208804aae373106d975186d
tlarbals824 <simkm0824@naver.com>
2023-03-16 19:56:44 +0900

    feat : add gradle setting

---
+++ src/main/java/com/kusithms/dongastd/DongaStDApplication.java
@@ -0,0 +1,13 @@
+package com.kusithms.dongastd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongaStDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongaStDApplication.class, args);
+	}
+
+}
