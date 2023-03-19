3a5e31f4e47f8de8bcc98b993b92452c6b86adf1
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-03-19 21:35:31 +0900

    feat: ContentRepo 생성 및 달력의 Content 시간 조회

---
+++ src/main/java/com/kusithms/dongastd/domain/content/service/ContentService.java
@@ -0,0 +1,26 @@
+package com.kusithms.dongastd.domain.content.service;
+
+import com.kusithms.dongastd.domain.content.entity.Content;
+import com.kusithms.dongastd.domain.content.repository.ContentRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ContentService {
+    private final ContentRepo contentRepo;
+
+    public List<LocalDateTime> findContentSchedule() {
+        List<Content> contents = contentRepo.findAll();
+        List<LocalDateTime> createTimes = new ArrayList<>();
+
+        for (int i = 0; i < contents.size(); i++) {
+            createTimes.add(contents.get(i).getCreatedDate());
+        }
+        return createTimes;
+    }
+}
