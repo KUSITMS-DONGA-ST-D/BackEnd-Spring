2d4319a62a5c8273e4719022aecc2a183e7b39a3
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-03-20 21:27:30 +0900

    delete: 소분류 삭제 및 컬럼 수정

--- src/main/java/com/kusithms/dongastd/domain/content/entity/Clinical.java
+++
@@ -1,6 +0,0 @@
-package com.kusithms.dongastd.domain.content.entity;
-
-// 임상 계열
-public enum Clinical implements MedicalType{
-    test
-}
--- src/main/java/com/kusithms/dongastd/domain/content/entity/Surgery.java
+++
@@ -1,7 +0,0 @@
-package com.kusithms.dongastd.domain.content.entity;
-
-
-// 외과
-public enum Surgery implements MedicalType{
-    test
-}
