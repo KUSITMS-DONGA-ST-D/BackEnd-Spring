89f8758dec48517add6ea7bed8bfab86c92802fd
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-03-20 23:36:28 +0900

    fix: formatter 수정

--- src/main/java/com/kusithms/dongastd/domain/livebroadcast/service/LiveBroadcastService.java
+++ src/main/java/com/kusithms/dongastd/domain/livebroadcast/service/LiveBroadcastService.java
@@ -22,7 +22,7 @@ public class LiveBroadcastService {
         List<String> liveStartTimes = new ArrayList<>();
 
         for (int i = 0; i < liveBroadcasts.size(); i++) {
-            String date = liveBroadcasts.get(i).getStartTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm"));
+            String date = liveBroadcasts.get(i).getStartTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
             liveStartTimes.add(date);
         }
 
