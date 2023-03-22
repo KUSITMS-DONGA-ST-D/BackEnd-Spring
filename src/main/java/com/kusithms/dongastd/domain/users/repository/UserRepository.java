838dfbaa4910ba4763333a5976640b07aeedbf85
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-03-22 12:18:46 +0900

    fix: conflict 해결

--- src/main/java/com/kusithms/dongastd/domain/users/repository/UserRepository.java
+++ src/main/java/com/kusithms/dongastd/domain/users/repository/UserRepository.java
@@ -1,15 +1,20 @@
 package com.kusithms.dongastd.domain.users.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import com.kusithms.dongastd.domain.users.entity.Gender;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kusithms.dongastd.domain.users.entity.User;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findAllByAge(int age);
-    List<User> findAllByAgeAndGender(int age, Gender gender);
-    List<User> findAllByGenderAndCategory(Gender gender, String category);
-    List<User> findAllByAgeIsBetweenAndGenderAndCategory(int startAge, int endAge, Gender gender, String category);
+
+    Page<User> findByCreatedDateBefore(LocalDateTime localDateTime, Pageable pageable);
+
+    List<User> findByCreatedDateBefore(LocalDateTime localDateTime);
+
+    List<User> findByCreatedDateBetween(LocalDateTime start, LocalDateTime end);
 }
