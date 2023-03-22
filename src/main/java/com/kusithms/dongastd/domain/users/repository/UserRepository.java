c76091e9592b0f53d0bbdd3a6c73968b6f81bf6b
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-03-22 17:51:12 +0900

    fix: conflict 해결

--- src/main/java/com/kusithms/dongastd/domain/users/repository/UserRepository.java
+++ src/main/java/com/kusithms/dongastd/domain/users/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.kusithms.dongastd.domain.users.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.kusithms.dongastd.domain.users.entity.Gender;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,9 +12,18 @@ import com.kusithms.dongastd.domain.users.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	Page<User> findByCreatedDateBefore(LocalDateTime localDateTime, Pageable pageable);
+    Page<User> findByCreatedDateBefore(LocalDateTime localDateTime, Pageable pageable);
 
-	List<User> findByCreatedDateBefore(LocalDateTime localDateTime);
+    List<User> findByCreatedDateBefore(LocalDateTime localDateTime);
 
-	List<User> findByCreatedDateBetween(LocalDateTime start, LocalDateTime end);
+    List<User> findByCreatedDateBetween(LocalDateTime start, LocalDateTime end);
+
+    List<User> findAllByAge(int age);
+
+    List<User> findAllByAgeAndGender(int age, Gender gender);
+
+    List<User> findAllByGenderAndCategory(Gender gender, String category);
+
+    List<User> findAllByAgeIsBetweenAndGenderAndCategory(int startAge, int endAge, Gender gender, String category);
+
 }
